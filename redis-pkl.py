29ae905b6b43fc8999d11a6d2514435b1b4db214
Scott Orr <orr_scott@bah.com>
2015-08-31 01:20:19 -0400

    Initial commit

---
+++ redis-pkl.py
@@ -0,0 +1,16 @@
+from redis import StrictRedis
+import cPickle as pickle
+
+# This sub-class of the StrictRedis class incorporates pickling and unpickling
+# through "pget" and "pset" methods that replace the "get" and "set" methods,
+# thereby enabling the caching of complex object classes.
+
+class PickledRedis(StrictRedis):
+
+    def pset(self, key, value, ex=None, px=None, nx=False, xx=False):
+        value_pickled = pickle.dumps(value, 2)
+        return self.set(key, value_pickled, ex=None, px=None, nx=False, xx=False)    
+        
+    def pget(self, key):
+        value_pickled = self.get(key)
+        return pickle.loads(value_pickled)
